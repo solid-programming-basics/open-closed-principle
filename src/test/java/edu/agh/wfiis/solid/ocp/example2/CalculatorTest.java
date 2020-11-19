32539ecb633553a048172ad58f7007c2e0cab894
Adrian Furman <fadikk367@gmail.com>
2020-11-19 16:52:30 +0100

    Add few more unit tests for Calculator

--- src/test/java/edu/agh/wfiis/solid/ocp/example2/CalculatorTest.java
+++ src/test/java/edu/agh/wfiis/solid/ocp/example2/CalculatorTest.java
@@ -11,19 +11,40 @@ public class CalculatorTest {
     private final Calculator underTest = new Calculator();
 
     @Test
-    public void shouldAddValues(){
-        int result = underTest.calculate(new String[]{"1", "+", "2"});
-        Assert.assertEquals(3,result);
+    public void shouldAddValues() {
+        int result = underTest.calculate(new String[] {"1", "+", "2"});
+        Assert.assertEquals(3, result);
     }
 
     @Test
-    public void shouldSubtractValues(){
-        int result = underTest.calculate(new String[]{"1", "-", "2"});
-        Assert.assertEquals(-1,result);
+    public void shouldSubtractValues() {
+        int result = underTest.calculate(new String[] {"1", "-", "2"});
+        Assert.assertEquals(-1, result);
+    }
+
+    @Test
+    public void shouldMultiplyValues() {
+        int result = underTest.calculate(new String[] {"4", "*", "6"});
+        Assert.assertEquals(24, result);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void shouldFailWhenUnsupportedOperandPassed(){
-        underTest.calculate(new String[]{"1", "/", "2"});
+    public void shouldFailWhenUnsupportedOperandPassed() {
+        underTest.calculate(new String[] {"1", "/", "2"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenTooFewArgumentsPassed() {
+        underTest.calculate(new String[] {"1", "+"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenTooManyArgumentsPassed() {
+        underTest.calculate(new String[] {"1", "-", "4", "7"});
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void shouldFailWhenArgumentCannotBeConvertedToNumber() {
+        underTest.calculate(new String[] {"a", "+", "4"});
     }
 }
