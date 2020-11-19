e79706cd4444383c53e62ad5fe4573bf69365d7c
miczoc98 <miczoc98@gmail.com>
2020-11-19 10:34:38 +0100

    Calculator refactoring.

--- src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
@@ -1,27 +1,86 @@
 package edu.agh.wfiis.solid.ocp.example2;
 
+import java.util.Map;
+
 public class Calculator {
+    private Map<String, Calculateable> operationsByOperator = Map.of(
+        "+", new Addition(),
+        "-", new Subtraction()
+    );
 
+    private int value1;
+    private int value2;
+    private String operator;
+
+    public int calculateResult(String[] args){
+        parse(args);
+        return operationsByOperator.get(operator).calculate(value1, value2);
+    }
+
+    @Deprecated
     public int calculate(String[] args) {
-        int val1 = Integer.valueOf(args[0]);
-        int val2 = Integer.valueOf(args[2]);
-        String operator = args[1];
+        int result = calculateResult(args);
+        System.out.println(result);
+        return result;
+    }
 
-        int result;
-        if ("+".equals(operator)) {
-            result = val1 + val2;
-            System.out.println(result);
-            return result;
-        } else if ("-".equals(operator)) {
-            result = val1 - val2;
-            System.out.println(result);
-            return result;
+    private void parse(String[] args)
+    {
+        Parser parser = new Parser(args);
+        operator = parser.getOperator();
+
+        if (!operationsByOperator.containsKey(operator)){
+            throw new IllegalArgumentException(operator + " is not supported");
         }
-        throw new IllegalArgumentException(operator + " is not supported");
+
+        value1 = parser.getVal1();
+        value2 = parser.getVal2();
     }
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         calculator.calculate(args);
+    }
+}
+
+
+class Parser{
+    private String[] operatorAndValues;
+
+    Parser(String[] args){
+        this.operatorAndValues = args;
+    }
+
+    public int getVal1(){
+        return Integer.valueOf(operatorAndValues[0]);
+    }
+
+    public int getVal2(){
+        return Integer.valueOf(operatorAndValues[2]);
+    }
+
+    public String getOperator(){
+        return operatorAndValues[1];
+    }
+}
+
+
+interface Calculateable{
+    public int calculate(int val1, int val2);
+}
+
+
+class Addition implements Calculateable{
+    @Override
+    public int calculate(int val1, int val2)
+    {
+        return val1 + val2;
+    }
+}
+
+class Subtraction implements Calculateable{
+    @Override
+    public int calculate(int val1, int val2){
+        return val1 - val2;
     }
 }
