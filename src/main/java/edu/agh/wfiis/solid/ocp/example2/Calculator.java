3ba1ffc21499beafc350d870e7b4d53480777eb6
pawel2000pl <pawelek24@op.pl>
2020-11-20 21:47:53 +0100

    Paweł Bielecki

--- src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
@@ -1,27 +1,129 @@
-package edu.agh.wfiis.solid.ocp.example2;
+//package edu.agh.wfiis.solid.ocp.example2;
+import java.util.*;
+import java.lang.*;
 
-public class Calculator {
+abstract class Operator 
+{
+    abstract double calc(double a, double b);
+}
 
-    public int calculate(String[] args) {
-        int val1 = Integer.valueOf(args[0]);
-        int val2 = Integer.valueOf(args[2]);
-        String operator = args[1];
-
-        int result;
-        if ("+".equals(operator)) {
-            result = val1 + val2;
-            System.out.println(result);
-            return result;
-        } else if ("-".equals(operator)) {
-            result = val1 - val2;
-            System.out.println(result);
-            return result;
-        }
-        throw new IllegalArgumentException(operator + " is not supported");
+public class Calculator 
+{
+    public HashMap<String, Operator> operators = new HashMap<>();
+    
+    ///tą funkcją programista może sprawdzić, czy dobrze nazwał klasę do automatycznej rejestracji. Służy także do rozwikłania nazwy klasy operatora
+    public static String createOperatorName(String operator)
+    {
+        if (operator.length() == 1)
+            return "Operator" + ((int)operator.charAt(0));
+            else
+            return "Operator_" + operator;
     }
 
-    public static void main(String[] args) {
+    ///sprawdza, czy znaleziona klasa jest odpowiedniego typu - jeśli tak, to ją zwraca, jeśli nie, zwraca null
+    Operator validateOperator(Object testObject)
+    {
+        if (testObject != null && (testObject instanceof Operator))
+            return (Operator)(testObject);
+        return null;
+    }
+    
+    ///funkcja umożliwia dodanie własnych operatorów (jeśli kogoś to interesuje), o ile taki został podany (bo można podać null)
+    public void registerOperator(String name, Operator operator)
+    {
+        if (operator != null)
+            operators.putIfAbsent(name, operator);
+    }
+    
+    ///automatyczne szukanie operatorów na podstawie przyjętej w createOperatorName() nomenklarury nazw
+    public Operator searchOperator(String operatorName)
+    {
+        try
+        {
+            return validateOperator(Class.forName(createOperatorName(operatorName)).newInstance());
+        } 
+        catch (Exception e) 
+        {
+            return null;
+        }   
+    }
+     
+    ///zwraca operator z zarejestrowanych, jeśli go tam nie ma, to szuka i rejestruje (i go zwraca z zarejestrowanych), jeśli nie może znaleźć, to zwraca null
+    public Operator getOperator(String operatorName)
+    {
+        if (!operators.containsKey(operatorName))        
+            registerOperator(operatorName, searchOperator(operatorName));
+        return operators.get(operatorName);
+    }
+     
+    //liczy
+    public double calculate(double val1, String operator, double val2)
+    {
+        Operator operatorObject = getOperator(operator);
+        if (operatorObject == null) 
+            throw new IllegalArgumentException(operator + " is not supported");
+            else
+            return operatorObject.calc(val1, val2);
+    }
+     
+    ///liczy na podstawie parametrów, o ile to możliwe
+    public double calculate(String[] args) 
+    {
+        if (args.length != 3)
+        {
+            System.out.println("Too much or not enought parameters");
+            return 0;
+        }
+        return calculate(Double.valueOf(args[0]), args[1], Double.valueOf(args[2]));
+    }
+
+    public static void main(String[] args) 
+    {
         Calculator calculator = new Calculator();
-        calculator.calculate(args);
+        System.out.println(calculator.calculate(args));
     }
 }
+
+class Addition extends Operator
+{
+    public double calc(double a, double b)
+    {
+        return a+b;
+    }
+}
+
+class Operator43 extends Addition {}    //java Calculator 2 '+' 3
+class Operator_plus extends Addition {} //java Calculator 2 plus 3
+
+
+class Subtraction extends Operator
+{
+    public double calc(double a, double b)
+    {
+        return a-b;
+    }
+}
+
+class Operator45 extends Subtraction {}     //java Calculator 2 '-' 3
+class Operator_minus extends Subtraction {} //java Calculator 2 minus 3
+
+class Multiplication extends Operator
+{
+    public double calc(double a, double b)
+    {
+        return a*b;
+    }
+}
+
+class Operator42 extends Multiplication {}      //java Calculator 2 '*' 3
+class Operator_times extends Multiplication {}  //java Calculator 2 times 3
+
+class Division extends Operator
+{
+    public double calc(double a, double b)
+    {
+        return a/b;
+    }
+}
+
+class Operator47 extends Division {}        //java Calculator 2 '\' 3
