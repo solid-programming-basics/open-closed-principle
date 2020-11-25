505db96b64733d2d73a3881df0db7bfa20d15ae2
Marcin Filipek <56352792+Fillipek@users.noreply.github.com>
2020-11-25 12:04:55 +0100

    Upload my solution

--- src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
@@ -1,27 +1,57 @@
 package edu.agh.wfiis.solid.ocp.example2;
 
-public class Calculator {
+import java.util.HashMap;
 
-    public int calculate(String[] args) {
-        int val1 = Integer.valueOf(args[0]);
-        int val2 = Integer.valueOf(args[2]);
-        String operator = args[1];
-
-        int result;
-        if ("+".equals(operator)) {
-            result = val1 + val2;
-            System.out.println(result);
-            return result;
-        } else if ("-".equals(operator)) {
-            result = val1 - val2;
-            System.out.println(result);
-            return result;
-        }
-        throw new IllegalArgumentException(operator + " is not supported");
+public class Calculator
+{
+    {
+        if (availableOperations == null) 
+        {
+            availableOperations = new HashMap<String, Operation>();
+            addOperation("+", (a,b)->a+b);
+            addOperation("-", (a,b)->a-b);
+        }  
     }
 
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.calculate(args);
+    private static HashMap<String, Operation> availableOperations;
+
+    public int calculate(String[] args)
+    {
+        Operation operation = tryGetOperation(args[1]);
+        int[]arguments = parseArguments(args);
+        return operation.execute(arguments[0], arguments[1]);
+    }
+
+    private int[] parseArguments(String[] args)
+    {
+        int[] argsParsed = new int[]
+        {
+            Integer.valueOf(args[0]),
+            Integer.valueOf(args[2])
+        };
+        return argsParsed;
+    }
+
+    private Operation tryGetOperation(String operator)
+    {
+        Operation operation = availableOperations.get(operator);
+        if (operation == null) 
+        {
+            throw new IllegalArgumentException(operator + " is not supported");
+        }
+        return operation;
+    }
+
+    private void addOperation(String operator, Operation operation)
+    {
+        if (availableOperations == null) 
+        {
+            throw new NullPointerException("Map of operations not initialized.");
+        }
+        if (availableOperations.get(operator) != null)
+        {
+            throw new IllegalArgumentException("Operator \"" + operator + "\" already exists.");
+        }
+        availableOperations.put(operator, operation);
     }
 }
---
+++ src/main/java/edu/agh/wfiis/solid/ocp/example2/Operation.java
@@ -0,0 +1,7 @@
+package edu.agh.wfiis.solid.ocp.example2;
+
+@FunctionalInterface
+interface Operation
+{
+    public int execute(int arg1, int arg2);
+}
