182fe51417bda2faa48a6a85f9dda20a954ded20
simswissie <simswissie@gmail.com>
2021-12-15 22:50:04 +0100

    Calculator refactoring

--- src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
@@ -1,27 +1,74 @@
 package edu.agh.wfiis.solid.ocp.example2;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Calculator {
+public class Calculator 
+{
+    private final Map<String, AritmeticOperation> givenOperations = new HashMap<>()
+    {{
+        put("+", new Add());
+        put("-", new Subtract());
+    }};
 
     public int calculate(String[] args) {
-        int val1 = Integer.valueOf(args[0]);
-        int val2 = Integer.valueOf(args[2]);
-        String operator = args[1];
+        AritmeticOperationArguments arguments = Parser.parse(args);
 
-        int result;
-        if ("+".equals(operator)) {
-            result = val1 + val2;
-            System.out.println(result);
-            return result;
-        } else if ("-".equals(operator)) {
-            result = val1 - val2;
-            System.out.println(result);
+        if(givenOperations.containsKey(arguments.aperation))
+        {
+            Double result = givenOperations.get(arguments.operation).calculate(arguments.firstValue, arguments.secondValue);
             return result;
         }
-        throw new IllegalArgumentException(operator + " is not supported");
+        else
+        {
+            throw new IllegalArgumentException(arguments.operation + " is not supported");
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Calculator calculator = new Calculator();
         calculator.calculate(args);
     }
+}
+
+interface AritmeticOperation
+{
+    Double calculate(Double firstValue, Double secondValue);
+}
+
+class Add implements AritmeticOperation
+{
+    @Override
+    public Double calculate(Double firstValue, Double secondValue)
+    {
+        return firstValue + secondValue;
+    }
+}
+
+class Subtract implements AritmeticOperation
+{
+    @Override
+    public Double calculate(Double firstValue, Double secondValue)
+    {
+        return firstValue - secondValue;
+    }
+}
+
+final class Parser
+{
+    public static AritmeticOperationArguments parse(String[] args)
+    {
+        AritmeticOperationArguments arguments = new AritmeticOperationArguments();
+        arguments.firstValue = Double.parseDouble(args[0]);
+        arguments.secondValue = Double.parseDouble(args[2]);
+        arguments.operation = args[1];
+        return arguments;
+    }
+}
+
+class AritmeticOperationArguments
+{
+    public Double firstValue;
+    public Double secondValue;
+    public Double operation;
 }
