757b3298e227ff49dca6febee8f7b836dc57b033
asaganowski13579 <56121222+asaganowski13579@users.noreply.github.com>
2022-01-05 17:13:25 +0100

    Update Calculator.java

--- src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
+++ src/main/java/edu/agh/wfiis/solid/ocp/example2/Calculator.java
@@ -1,19 +1,29 @@
 package edu.agh.wfiis.solid.ocp.example2;
 
 public class Calculator {
-
+    
+    private int subtraction(int value1, int value2){
+        return value1-value2;
+    }
+        
+    private int addition (int value1, int value2){
+        return value1+value2;
+    }
+        
+        
     public int calculate(String[] args) {
+        
         int val1 = Integer.valueOf(args[0]);
         int val2 = Integer.valueOf(args[2]);
         String operator = args[1];
-
+        
         int result;
         if ("+".equals(operator)) {
-            result = val1 + val2;
+            result=addition(val1, val2);
             System.out.println(result);
             return result;
         } else if ("-".equals(operator)) {
-            result = val1 - val2;
+            result=subtraction(val1, val2);
             System.out.println(result);
             return result;
         }
